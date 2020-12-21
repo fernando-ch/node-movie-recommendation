50d6cca769b9cc74e04abe444da8c531f297bbad
Fernando <fandradecamacho@gmail.com>
2020-12-21 19:51:25 -0300

    Add server port to environment variable

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws')
 const db = require('./database')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const wsServer = new WebSocket.Server({ noServer: true })
 
 app.use(express.json())
